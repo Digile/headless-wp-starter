c4afc546e21d9383e65772dede66332543f8b545
eherrmann <emanuelherrmann@gmail.com>
2018-09-07 14:45:09 +1200

    try multiple domains for cors on wp api

--- wordpress/wp-content/themes/postlight-headless-wp/inc/cors.php
+++ wordpress/wp-content/themes/postlight-headless-wp/inc/cors.php
@@ -8,11 +8,15 @@ add_action( 'rest_api_init', function () {
     remove_filter( 'rest_pre_serve_request', 'rest_send_cors_headers' );
 
     add_filter( 'rest_pre_serve_request', function ( $value ) {
-        header( 'Access-Control-Allow-Origin: ' . get_frontend_origin() );
-        header( 'Access-Control-Allow-Origin: ' . 'http://localhost:3100' );
+        $http_origin = $_SERVER['HTTP_ORIGIN'];
+
+        if ($http_origin == "http://localhost:3000" || $http_origin == "http://localhost:3100" || $http_origin == "http://www.domain3.com")
+        {  
         
-        header( 'Access-Control-Allow-Methods: GET' );
-        header( 'Access-Control-Allow-Credentials: true' );
-        return $value;
+            header( 'Access-Control-Allow-Origin: ' . $http_origin );
+            header( 'Access-Control-Allow-Methods: GET' );
+            header( 'Access-Control-Allow-Credentials: true' );
+            return $value;
+        }
     });
 }, 15 );
