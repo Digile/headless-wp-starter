703addd087cb82593883e3a560f25286a842f9f8
eherrmann <emanuelherrmann@gmail.com>
2018-10-11 11:48:00 +1300

    missing function for preview url (was breaking the post edit in admin)

--- wordpress/wp-content/themes/postlight-headless-wp/inc/frontend-origin.php
+++ wordpress/wp-content/themes/postlight-headless-wp/inc/frontend-origin.php
@@ -10,3 +10,16 @@
 function allowed_forntend_origins() {
     return ['http://localhost:3000','http://localhost:3100','https://nextjs-app-ttkioibrnq.now.sh'];
 }
+
+
+/**
+ * Placeholder function for determining the frontend origin.
+ *
+ * @TODO Determine the headless client's URL based on the current environment.
+ * ALso needs to configure the front end origin in an env file
+ *
+ * @return str Frontend origin URL, i.e., http://localhost:3000.
+ */
+function get_frontend_origin() {
+    return 'http://localhost:3000';
+}
