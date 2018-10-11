1b3036c43794ccfef108780511a3b962af077c46
eherrmann <emanuelherrmann@gmail.com>
2018-10-11 14:11:11 +1300

    test api endpoint for post with interests

--- wordpress/wp-content/themes/postlight-headless-wp/functions.php
+++ wordpress/wp-content/themes/postlight-headless-wp/functions.php
@@ -50,12 +50,45 @@ function wpse28782_remove_menu_items() {
 
 add_action( 'admin_menu', 'wpse28782_remove_menu_items' );
 
-
-function test_query_vars ( $vars ) {
-  $vars[] = 'meta_query';
-  return $vars;
+/**
+ * Plugin Name: WP REST API filter parameter
+ * Description: This plugin adds a "filter" query parameter to API post collections to filter returned results based on public WP_Query parameters, adding back the "filter" parameter that was removed from the API when it was merged into WordPress core.
+ * Author: WP REST API Team
+ * Author URI: http://v2.wp-api.org
+ * Version: 0.1
+ * License: GPL2+
+ **/
+add_action( 'rest_api_init', 'rest_api_filter_add_filters' );
+ /**
+  * Add the necessary filter to each post type
+  **/
+function rest_api_filter_add_filters() {
+	foreach ( get_post_types( array( 'show_in_rest' => true ), 'objects' ) as $post_type ) {
+		add_filter( 'rest_' . $post_type->name . '_query', 'rest_api_filter_add_filter_param', 10, 2 );
+	}
 }
-
-add_filter( 'rest_query_vars', 'test_query_vars' );
-   
-
+/**
+ * Add the filter parameter
+ *
+ * @param  array           $args    The query arguments.
+ * @param  WP_REST_Request $request Full details about the request.
+ * @return array $args.
+ **/
+function rest_api_filter_add_filter_param( $args, $request ) {
+	// Bail out if no filter parameter is set.
+	if ( empty( $request['filter'] ) || ! is_array( $request['filter'] ) ) {
+		return $args;
+	}
+	$filter = $request['filter'];
+	if ( isset( $filter['posts_per_page'] ) && ( (int) $filter['posts_per_page'] >= 1 && (int) $filter['posts_per_page'] <= 100 ) ) {
+		$args['posts_per_page'] = $filter['posts_per_page'];
+	}
+	global $wp;
+	$vars = apply_filters( 'rest_query_vars', $wp->public_query_vars );
+	foreach ( $vars as $var ) {
+		if ( isset( $filter[ $var ] ) ) {
+			$args[ $var ] = $filter[ $var ];
+		}
+	}
+	return $args;
+}
